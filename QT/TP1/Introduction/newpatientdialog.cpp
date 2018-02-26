e118d2e3f6d215be530e1f48f739df98edbeda94
Thomas COUCHOUD <zerderr@gmail.com>
2018-02-26 21:36:56 +0100

    Indicate problems when creating patient

--- QT/TP1/Introduction/newpatientdialog.cpp
+++ QT/TP1/Introduction/newpatientdialog.cpp
@@ -60,7 +60,78 @@ void NewPatientDialog::on_cityLineEdit_textEdited(const QString &arg1)
 
 void NewPatientDialog::on_okButton_clicked()
 {
-    if(!ui->lastNameLineEdit->text().isEmpty() && !ui->firstNameLineEdit->text().isEmpty() && !ui->addressLineEdit->text().isEmpty() && !ui->cityLineEdit->text().isEmpty() && ui->postalCodeLineEdit->text().size() == 5 && ui->dayOfConsultationDateEdit->date() >= QDate::currentDate() && ui->durationTimeEdit->time().minute() + ui->durationTimeEdit->time().hour() >= 1)
+    bool valid = true;
+    if(ui->lastNameLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->lastNameLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->lastNameLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->firstNameLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->firstNameLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->firstNameLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->addressLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->addressLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->addressLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->cityLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->cityLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->cityLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->postalCodeLineEdit->text().size() != 5)
+    {
+        valid = false;
+        ui->postalCodeLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->postalCodeLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->dayOfConsultationDateEdit->date() < QDate::currentDate())
+    {
+        valid = false;
+        ui->dayOfConsultationDateEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->dayOfConsultationDateEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->durationTimeEdit->time().minute() + ui->durationTimeEdit->time().hour() < 1)
+    {
+        valid = false;
+        ui->durationTimeEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->durationTimeEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(valid)
         accept();
 }
 
