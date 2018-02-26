c6126cea08365c134bb35ce4e3dffba52280d561
Thomas COUCHOUD <zerderr@gmail.com>
2018-02-26 21:40:55 +0100

    indicate problems when adding staff

--- QT/TP1/Introduction/newstaffdialog.cpp
+++ QT/TP1/Introduction/newstaffdialog.cpp
@@ -54,9 +54,53 @@ void NewStaffDialog::on_typeComboBox_currentIndexChanged(const QString &arg1)
 
 void NewStaffDialog::on_okButton_clicked()
 {
-    if(!ui->lastNameLineEdit->text().isEmpty() && !ui->firstNameLineEdit->text().isEmpty())
-        if(ui->typeComboBox->currentText() != "Informaticien" || (!ui->loginLineEdit->text().isEmpty() && !ui->passwordLineEdit->text().isEmpty()))
-            accept();
+    bool valid = true;
+    if(ui->lastNameLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->lastNameLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->lastNameLineEdit->setStyleSheet("background-color:white;");
+    }
+
+    if(ui->firstNameLineEdit->text().isEmpty())
+    {
+        valid = false;
+        ui->firstNameLineEdit->setStyleSheet("background-color:red;");
+    }
+    else
+    {
+        ui->firstNameLineEdit->setStyleSheet("background-color:white;");
+    }
+
+
+    if(ui->typeComboBox->currentText() == "Informaticien")
+    {
+        if(ui->loginLineEdit->text().isEmpty())
+        {
+            valid = false;
+            ui->loginLineEdit->setStyleSheet("background-color:red;");
+        }
+        else
+        {
+            ui->loginLineEdit->setStyleSheet("background-color:white;");
+        }
+
+        if(ui->passwordLineEdit->text().isEmpty())
+        {
+            valid = false;
+            ui->passwordLineEdit->setStyleSheet("background-color:red;");
+        }
+        else
+        {
+            ui->passwordLineEdit->setStyleSheet("background-color:white;");
+        }
+    }
+
+    if(valid)
+        accept();
 }
 
 void NewStaffDialog::on_cancelButton_clicked()
