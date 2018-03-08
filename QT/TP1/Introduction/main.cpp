6902bef7bb67cfd5a44c86cf0f556a9e7aac3d8d
Thomas COUCHOUD <zerderr@gmail.com>
2018-03-08 08:41:54 +0100

    Init the database

--- QT/TP1/Introduction/main.cpp
+++ QT/TP1/Introduction/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //std::cout << C_INIT_BD::Creation_BD() << std::endl;
+    std::cout << C_INIT_BD::Creation_BD() << std::endl;
 
     db = DBConnect();
 
