50b53625524a68961b30434eba1073830ac00399
Thomas COUCHOUD <zerderr@gmail.com>
2018-03-13 13:15:10 +0100

    Remove compiler warning

--- Projet/mainwindow.cpp
+++ Projet/mainwindow.cpp
@@ -244,6 +244,7 @@ void MainWindow::on_actionExit_triggered()
 
 void MainWindow::on_table_data_changed(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles)
 {
+    (void)roles; //Unused warning taken down
     if(topLeft.column() == bottomRight.column() && topLeft.row() == bottomRight.row())
     {
         QVariant q = model->data(topLeft);
